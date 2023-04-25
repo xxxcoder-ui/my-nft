000ff76138fc76cd26b7ebea78db1c15cea11461
xxxcoder-ui <yogamusik15@gmail.com>
2023-04-25 05:51:11 -0700

    nft
    nft

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
+module.exports = {
+ solidity: "0.8.1",
+ defaultNetwork: "arbitrum_goerli",
+ networks: {
+   hardhat: {},
+   arbitrum_goerli: {
+     chainId: 421613,
+     url: API_URL,
+     accounts: [`0x${PRIVATE_KEY}`],
+   },
+ },
+};
